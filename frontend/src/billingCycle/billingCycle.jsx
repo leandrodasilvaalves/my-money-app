b19c5db3fe0953729fd537322435bbeafe55b7b4
Leandro da Silva Alves <leandro.alves@invillia.com>
2020-03-08 18:22:28 -0300

    Listando ciclos de pagamento

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-export default props => (
-  <div>
-    <h1>BillingCycle</h1>
-  </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content';
+import Tabs from '../common/tab/tabs';
+import TabsHeader from '../common/tab/tabsHeader';
+import TabsContent from '../common/tab/tabsContent';
+import Tabheader from '../common/tab/tabHeader';
+import TabContent from '../common/tab/tabContent';
+import { selectTab, showTabs } from '../common/tab/tabActions';
+import List from './billingCycleList';
+
+class BillingCycle extends Component {
+
+  componentWillMount(){
+    this.props.selectTab('tabList');
+    this.props.showTabs('tabList', 'tabCreate');
+  }
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+        <Content>
+          <Tabs>
+            <TabsHeader>
+              <Tabheader label='Listar' icon='bars' target='tabList'/>
+              <Tabheader label='Incluir' icon='plus' target='tabCreate'/>
+              <Tabheader label='Alterar' icon='pencil' target='tabUpdate'/>
+              <Tabheader label='Excluir' icon='trash-o' target='tabDelete'/>
+            </TabsHeader>
+            <TabsContent>
+              <TabContent id='tabList'>
+                <List/>
+              </TabContent>
+              <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+              <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+              <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+            </TabsContent>
+          </Tabs>
+        </Content>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({selectTab, showTabs}, dispatch);
+
+export default connect(null,mapDispatchToProps)(BillingCycle);
--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -4,7 +4,7 @@ export default props => (
   <footer className="main-footer">
     <strong>
       Copyright &copy; 2020
-      <a href='http://github.com/leandrodasilvaalves' target='_blank'>Leandro Alves</a>
+      <a href='http://github.com/leandrodasilvaalves' target='_blank'> Leandro Alves</a>
     </strong>
   </footer>
 )
