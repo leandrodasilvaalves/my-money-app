f405544815e3332393beb7e2fcd9250542b0f8af
Leandro da Silva Alves <leandro.alves@invillia.com>
2020-03-08 09:57:31 -0300

    Estrutura criada

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2020
+      <a href='http://github.com/leandrodasilvaalves' target='_blank'>Leandro Alves</a>
+    </strong>
+  </footer>
+)
