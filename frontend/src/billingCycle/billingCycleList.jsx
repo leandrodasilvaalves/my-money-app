239347356f9f17ccc6f9990c086b032a1a0efff3
Leandro da Silva Alves <leandro.alves@invillia.com>
2020-03-17 20:33:10 -0300

    Excluindo dados

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { getList, showUpdate } from './billingCycleActions';
+import { getList, showUpdate, showDelete } from './billingCycleActions';
 
 class BillingCycleList extends Component {
 
@@ -21,6 +21,11 @@ class BillingCycleList extends Component {
             onClick={() => this.props.showUpdate(bc)}>
             <i className="fa fa-pencil"></i>
           </button>
+          <button className="btn btn-danger" 
+            onClick={() => this.props.showDelete(bc)}>
+            <i className="fa fa-trash-o"></i>
+          </button>
+
         </td>
       </tr>
     ));
@@ -52,6 +57,6 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ getList, showUpdate }, dispatch);
+  bindActionCreators({ getList, showUpdate, showDelete}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
